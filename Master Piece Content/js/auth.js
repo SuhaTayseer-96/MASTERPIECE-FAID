d031195d39b5067ef91084d68c9bc2201e21dacc
SuhaKrasneh <suha.kras967@gmail.com>
2024-10-30 09:20:25 +0300

    up30

--- Master Piece Content/js/auth.js
+++ Master Piece Content/js/auth.js
@@ -4,24 +4,33 @@ var profileLink2 = document.getElementById("profileLink2");
 var cartLink = document.getElementById("cartLink");
 
 function checkLoginStatus() {
+    debugger
     var userId = localStorage.getItem('userId');
-    var userEmail = localStorage.getItem('userEmail'); 
+    var userEmail = localStorage.getItem('email'); 
     
     if (userId) {
         authButton.innerText = "Logout";
         authButton.href = "#"; 
         authButton.addEventListener('click', logout); 
 
-     
+     debugger
+
+     if(userEmail === "s@admin.com"){
+        window.location.href = "/admin/html.phoenixcoded.net/light-able/bootstrap/application/account-profile.html";
+        return ;
+     }
+
         if (userEmail === "suha@user.com") {
             profileLink1.style.display = "inline-block"; 
+            cartLink.style.display = "inline-block";
+
         } else if (userEmail === "spoonpalace@rest.com") {
             profileLink2.style.display = "inline-block"; 
+            profileLink1.style.display = "none";
         }
 
-        cartLink.style.display = "inline-block";
-        profileLink1.style.display="inline-block";
-        profileLink1.style.display="inline-block";
+       // profileLink1.style.display="inline-block";
+       // profileLink1.style.display="inline-block";
     } else {
         authButton.innerText = "Join Us";
         authButton.href = "login.html"; 
@@ -35,8 +44,9 @@ function checkLoginStatus() {
 function logout() {
     localStorage.removeItem('userId');
     localStorage.removeItem('userEmail'); // Remove the email from localStorage as well
-    alert("Logged out Successfully");
+    alert("Are you sure you want to log out?");
     window.location.href = "index.html";
 }
 
 document.addEventListener('DOMContentLoaded', checkLoginStatus);
+checkLoginStatus()
