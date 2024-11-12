caadf443c8063c2df776cddda54d865b2f49727d
SuhaKrasneh <suha.kras967@gmail.com>
2024-11-12 18:26:50 +0300

    login, profile

--- Master Piece Content/js/auth.js
+++ Master Piece Content/js/auth.js
@@ -1,36 +1,34 @@
+// Get elements from the DOM
 var authButton = document.getElementById("authButton");
 var profileLink1 = document.getElementById("profileLink1");
 var profileLink2 = document.getElementById("profileLink2");
 var cartLink = document.getElementById("cartLink");
 
 function checkLoginStatus() {
-    debugger
+    debugger;
     var userId = localStorage.getItem('userId');
     var userEmail = localStorage.getItem('email'); 
-    
+
     if (userId) {
         authButton.innerText = "Logout";
         authButton.href = "#"; 
         authButton.addEventListener('click', logout); 
 
-     debugger
+        debugger;
 
-     if(userEmail === "s@admin.com"){
-        window.location.href = "admin/html.phoenixcoded.net/light-able/bootstrap/application/account-profile.html";
-        return ;
-     }
+        // Admin check
+        if (userEmail === "s@admin.com") {
+            window.location.href = "admin/html.phoenixcoded.net/light-able/bootstrap/application/account-profile.html";
+            return;
+        }
 
-        if (userEmail === "suha@user.com") {
+        if (userEmail && userEmail.endsWith("@user.com")) {
             profileLink1.style.display = "inline-block"; 
             cartLink.style.display = "inline-block";
-
-        } else if (userEmail === "spoonpalace@rest.com") {
+        } else if (userEmail && userEmail.endsWith("@rest.com")) {
             profileLink2.style.display = "inline-block"; 
             profileLink1.style.display = "none";
         }
-
-       // profileLink1.style.display="inline-block";
-       // profileLink1.style.display="inline-block";
     } else {
         authButton.innerText = "Join Us";
         authButton.href = "login.html"; 
@@ -43,10 +41,10 @@ function checkLoginStatus() {
 
 function logout() {
     localStorage.removeItem('userId');
-    localStorage.removeItem('userEmail'); // Remove the email from localStorage as well
+    localStorage.removeItem('email'); 
     alert("Are you sure you want to log out?");
     window.location.href = "index.html";
 }
 
 document.addEventListener('DOMContentLoaded', checkLoginStatus);
-checkLoginStatus()
+checkLoginStatus();
